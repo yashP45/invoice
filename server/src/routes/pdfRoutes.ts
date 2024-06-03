288307aee23228c7b39ad02b7992844d1b72b9a5
yash <yashporwal208@gmail.com>
2024-06-03 17:37:08 +0530

    gga

--- server/src/routes/pdfRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { generatePDF } from '../controllers/pdfController';
-import authMiddleware from '../middleware/authmiddleware';
-
-const router = express.Router();
-
-router.post('/generate-pdf', authMiddleware, generatePDF);
-
-export default router;
